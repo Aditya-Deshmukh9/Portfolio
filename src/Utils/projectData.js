f9ccaa33dfb3f2aaf75b1a4e2b64847ab75e9ce9
Aditya-Deshmukh9 <adityadesh937@gmail.com>
2024-02-12 21:25:57 +0530

    walmart project added

--- src/Utils/projectData.js
+++ src/Utils/projectData.js
@@ -1,14 +1,15 @@
 import drumKit from "../assets/DrumbKitGame.png";
 import Ecart from "../assets/Ecart Image.png";
+import walmart from "../assets/walmart.png";
 
 const projectData = [
   {
-    title: "Drum Kit Game",
-    image: drumKit,
+    title: "Walmart Clone Nextjs",
+    image: walmart,
     description:
-      'Using Html, Css and JavaScript, "Crafted an engaging Drum Kit Game for my portfolio using HTML, CSS, and JavaScript. Users can play virtual drums, enhancing interactivity and showcasing frontend development skills. Check it out!"',
-    githubLink: "https://github.com/Aditya-Deshmukh9/Drum-Kit-Project",
-    liveLink: "https://aditya-deshmukh9.github.io/Drum-Kit-Project/",
+      "Her I use Nextjs, Typescript, Tailwind css, Zustand for state management and Oxylabs for Scripting from Walmart original website, Check out Now!",
+    githubLink: "https://github.com/Aditya-Deshmukh9/walmart-clone",
+    liveLink: "https://walmartclone-by-aditya.vercel.app",
   },
   {
     title: "E-Cart",
@@ -24,15 +25,7 @@ const projectData = [
     description:
       'Using Html, Css and JavaScript, "Crafted an engaging Drum Kit Game for my portfolio using HTML, CSS, and JavaScript. Users can play virtual drums, enhancing interactivity and showcasing frontend development skills. Check it out!"',
     githubLink: "https://github.com/Aditya-Deshmukh9/Drum-Kit-Project",
-    liveLink: "#",
-  },
-   {
-    title: "Walmart Clone Nextjs",
-    image: walmart,
-    description:
-      'Her I use Nextjs, Typescript, Tailwind css, Zustand for state management and Oxylabs for Scripting from Walmart original website, Check out Now!',
-    githubLink: "https://github.com/Aditya-Deshmukh9/walmart-clone",
-    liveLink: "https://walmartclone-by-aditya.vercel.app",
+    liveLink: "https://aditya-deshmukh9.github.io/Drum-Kit-Project/",
   },
   // Add more project data as needed
 ];
